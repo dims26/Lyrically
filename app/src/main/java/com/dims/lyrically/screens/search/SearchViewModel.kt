ac65aec5061da02ff649dbf0444c15f0efd135f7
Adedimeji Sebiotimo <dims95@live.com>
2021-01-18 21:13:00 +0100

    feature(SearchViewModel):add and test functionality to retrieve cache list given query param

--- app/src/main/java/com/dims/lyrically/screens/search/SearchViewModel.kt
+++ app/src/main/java/com/dims/lyrically/screens/search/SearchViewModel.kt
@@ -3,21 +3,45 @@ package com.dims.lyrically.screens.search
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.dims.lyrically.models.SearchCache
 import com.dims.lyrically.utils.LoadState.*
 import com.dims.lyrically.models.Song
 import com.dims.lyrically.utils.LyricDataProvider
 import com.dims.lyrically.repository.Repository
 import com.dims.lyrically.utils.LoadState
+import kotlinx.coroutines.*
 
 class SearchViewModel(private val repo: Repository) : ViewModel() {
+
     private val _songs = mutableListOf<Song>()
     val songs: List<Song> get() = _songs
+
+    private val _searchCaches = mutableListOf<SearchCache>()
+    val searchCaches: List<SearchCache> get() = _searchCaches
 
     private val _loadingIndicator = MutableLiveData(IDLE)
     val loadingIndicator: LiveData<LoadState> get() = _loadingIndicator
 
+    private val _cacheLoadingIndicator = MutableLiveData(IDLE)
+    val cacheLoadingIndicator: LiveData<LoadState> get() = _cacheLoadingIndicator
+
     fun search(query: String, provider: LyricDataProvider) {
         _loadingIndicator.value = LOADING
         repo.search(query, _loadingIndicator, _songs, provider)
+    }
+
+    fun searchCache(query: String){
+        _cacheLoadingIndicator.value = LOADING
+
+        if (viewModelScope.coroutineContext.isActive)
+            viewModelScope.coroutineContext.cancel()
+
+        viewModelScope.launch {
+            delay(1000)
+            _searchCaches.clear()
+            _searchCaches.addAll(repo.getCaches(query))
+            _cacheLoadingIndicator.postValue(LOADED)
+        }
     }
 }
--- app/src/test/java/com/dims/lyrically/viewmodels/SearchViewModelTest.kt
+++ app/src/test/java/com/dims/lyrically/viewmodels/SearchViewModelTest.kt
@@ -3,51 +3,81 @@ package com.dims.lyrically.viewmodels
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
+import com.dims.lyrically.models.SearchCache
 import com.dims.lyrically.models.Song
 import com.dims.lyrically.repository.Repository
 import com.dims.lyrically.screens.search.SearchViewModel
 import com.dims.lyrically.utils.LoadState
 import com.jraska.livedata.test
 import com.nhaarman.mockitokotlin2.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runBlockingTest
+import kotlinx.coroutines.test.setMain
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.Matchers.equalTo
+import org.junit.After
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertThat
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
+@ExperimentalCoroutinesApi
 class SearchViewModelTest {
 
     @get:Rule
     val instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    private val mainThreadSurrogate = TestCoroutineDispatcher()
+    private lateinit var repository : Repository
+    private lateinit var viewModel: SearchViewModel
 
     private val songList = listOf(
             Song("..", "..", "..", "..", "..", 1, ".."),
             Song("..", "..", "..", "..", "..", 2, "..")
     )
 
+    private val searchCaches = listOf(
+            SearchCache(1, "..", "..", "..", "..", "..", ".."),
+            SearchCache(1, "..", "..", "..", "..", "..", "..")
+    )
+
+    @Before
+    fun setUp() {
+        Dispatchers.setMain(mainThreadSurrogate)
+        repository = mock()
+        viewModel = SearchViewModel(repository)
+    }
+
+    @After
+    fun tearDown() {
+        Dispatchers.resetMain() // reset main dispatcher to the original Main dispatcher
+    }
+
     @Test
-    fun test_loadingIndicator_onClassInit() = runBlocking{
-        val repository = mock<Repository>()
+    fun test_loadingIndicators_IdleOnClassInit() = runBlocking{
         val expected = LoadState.IDLE
-        val viewModel = SearchViewModel(repository)
 
-        val actual = viewModel.loadingIndicator.test().value()
+        val actual1 = viewModel.loadingIndicator.test().value()
+        val actual2 = viewModel.loadingIndicator.test().value()
 
-        assertEquals(expected, actual)
+        assertThat(expected, allOf(equalTo(actual1), equalTo(actual2)))
     }
 
     @Test
     fun test_search_SuccessfulLoad() = runBlocking{
         val indicatorObserver = mock<Observer<LoadState>>()
-        val repository = mock<Repository>{repository ->
-            on(repository.search(any(), any(), any(), any()))
-                    .doAnswer {
-                        val songs = it.getArgument<MutableList<Song>>(2)
-                        songs.clear()
-                        songs.addAll(songList)
-                        it.getArgument<MutableLiveData<LoadState>>(1).postValue(LoadState.LOADED)
-            }
-        }
-        val viewModel = SearchViewModel(repository)
+        whenever(repository.search(any(), any(), any(), any()))
+                .thenAnswer {
+                    val songs = it.getArgument<MutableList<Song>>(2)
+                    songs.clear()
+                    songs.addAll(songList)
+                    it.getArgument<MutableLiveData<LoadState>>(1).postValue(LoadState.LOADED)
+                }
         viewModel.loadingIndicator.observeForever(indicatorObserver)
         val expected = songList
 
@@ -62,13 +92,9 @@ class SearchViewModelTest {
     @Test
     fun test_search_Error() = runBlocking{
         val indicatorObserver = mock<Observer<LoadState>>()
-        val repository = mock<Repository>{repository ->
-            on(repository.search(any(), any(), any(), any()))
-                    .doAnswer {
-                        it.getArgument<MutableLiveData<LoadState>>(1).postValue(LoadState.ERROR)
-                    }
+        whenever(repository.search(any(), any(), any(), any())).thenAnswer {
+            it.getArgument<MutableLiveData<LoadState>>(1).postValue(LoadState.ERROR)
         }
-        val viewModel = SearchViewModel(repository)
         viewModel.loadingIndicator.observeForever(indicatorObserver)
         val expected = listOf<Song>()
 
@@ -78,5 +104,22 @@ class SearchViewModelTest {
         verify(indicatorObserver, never()).onChanged(LoadState.LOADED)
         verify(indicatorObserver).onChanged(LoadState.ERROR)
         assertEquals(viewModel.songs, expected)
+    }
+
+    @Test
+    fun test_searchCache() = runBlockingTest {
+        val cacheLoadIndicatorObserver = mock<Observer<LoadState>>()
+        val query = ""
+        val expected = searchCaches
+        whenever(repository.getCaches(eq(query))).thenReturn(expected)
+        viewModel.cacheLoadingIndicator.observeForever(cacheLoadIndicatorObserver)
+
+        viewModel.searchCache(query)
+
+        mainThreadSurrogate.advanceTimeBy(1500)
+        verify(repository).getCaches(eq(query))
+        verify(cacheLoadIndicatorObserver, times(1)).onChanged(LoadState.LOADING)
+        verify(cacheLoadIndicatorObserver, times(1)).onChanged(LoadState.LOADED)
+        assertEquals(viewModel.searchCaches, expected)
     }
 }
