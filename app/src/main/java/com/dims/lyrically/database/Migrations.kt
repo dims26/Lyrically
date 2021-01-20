230cf8d6934a785628b9066fef8e9bf7729a9dfb
Adedimeji Sebiotimo <dims95@live.com>
2021-01-21 00:12:23 +0100

    fix(Migrations):fix migration_1_2 sql statement to better reflect SearchCache model

--- app/src/main/java/com/dims/lyrically/database/Migrations.kt
+++ app/src/main/java/com/dims/lyrically/database/Migrations.kt
@@ -5,6 +5,6 @@ import androidx.sqlite.db.SupportSQLiteDatabase
 
 val MIGRATION_1_2 = object : Migration(1, 2) {
     override fun migrate(database: SupportSQLiteDatabase) {
-        database.execSQL("CREATE TABLE `search_cache` (`id` INTEGER, `full_title` TEXT, `title` TEXT, `thumbnail_url` TEXT, `url` TEXT, `title_featured` TEXT, `artist_name` TEXT, PRIMARY KEY(`id`))")
+        database.execSQL("CREATE TABLE IF NOT EXISTS `search_cache` (`id` INTEGER NOT NULL, `full_title` TEXT NOT NULL, `title` TEXT NOT NULL, `thumbnail_url` TEXT NOT NULL, `url` TEXT NOT NULL, `title_featured` TEXT NOT NULL, `artist_name` TEXT NOT NULL, PRIMARY KEY(`id`))")
     }
 }
